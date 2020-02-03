5b0ec306be1a1a9e3dfd9ca81b686c57289c9944
Masaya-Watanabe-0206 <60571336+Masaya-Watanabe-0206@users.noreply.github.com>
2020-02-03 18:21:20 +0900

    Update urls.py

--- material_database/album/urls.py
+++ material_database/album/urls.py
@@ -8,17 +8,17 @@ urlpatterns = [
     path('file/', views.FileListView.as_view(), name='file_list'),
 
     # 通常のフォーム
-    path('single/upload/', views.SingleUploadView.as_view(), name='single_upload'),
+    path('single/upload/', views.SingleView.as_view(), name='single_upload'),
 
     # モデルフォーム
-    path('single/upload/model/', views.SingleUploadWithModelView.as_view(), name='single_upload_with_model'),
+    path('single/upload/model/', views.SingleWithModelView.as_view(), name='single_upload_with_model'),
 
 
-    path('multi/upload/', views.MultiUploadView.as_view(), name='multi_upload'),
+    path('multi/upload/', views.MultiView.as_view(), name='multi_upload'),
 
 
     path('multi/upload/model/', views.multi_upload_with_model, name='multi_upload_with_model'),
 
 
-    path('input/multiple/upload/', views.InputMultiUploadView.as_view(), name='input_multi'),
+    path('input/multiple/upload/', views.InputMultiView.as_view(), name='input_multi'),
 ]
